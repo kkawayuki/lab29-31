7db75107a2b7d983959ab97b80a5ac3b16722a29
kkawayuki <kyukiokawa@gmail.com>
2024-11-06 20:21:11 -0800

    psudeocode

--- main.cpp
+++ main.cpp
@@ -1,1 +1,43 @@
-//psudeocode
+//Include necessary libraries
+//Include necessary header file(s)
+//Namespace std
+
+//function prototypes for various necessary parts of program
+    //day running function (facilitates changes)
+    //helper function for printing formatted data
+    //function for tracking trade volume
+    //function for tracking stock price
+    //function for tracking sector-wide effects
+
+    //function parameters will depend on whether they reference or change fields
+        //reference vs value
+
+//define main()
+    //initalize a map named something like "notRobinhood" to store data about various sector(s) arrays
+        //each sector array associated a list of three companies
+    
+    //open an external file to read in data about the companies and populate their associated list entries
+        //include error handling/messaging
+        //close once all data has been sucessfully read in
+    
+    //begin the time-based for loop simulating a month's market fluctuations
+        // 30ish calls to the day running function 
+            //for each array (sector)
+                //facilitates any sector-wide changes
+                //for each list (stock)
+                    //facilitates data changes (stock price increase/decreate), (trading volume increase/decrease)
+                    //data printing, ex: [Day X: APPL: 231.23, VOLUME: +232,323]
+                    //similar printing that announces sector-wide events
+                        //could even get crazy and do whole-market effects (in spirit of the recent election)
+                
+    // pause briefly/add space between messages to maintain readable breakdown
+
+//end main()
+    
+//function implementation:
+    //day running function (facilitates changes)
+    //helper function for printing formatted data
+    //function for tracking trade volume
+    //function for tracking stock price
+    //function for tracking sector-wide effects
+
